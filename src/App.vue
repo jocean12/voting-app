79186229d4ad9a168242d49de72310cc52695038
jocean12 <joe@sheehanweb.com>
2022-03-04 14:43:29 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <form>
+    <div class="center">
+      <label><h1 class="mainQ">What's your favorite JavaScript Framework?</h1></label>
+      <input type="radio" value="Vue" v-model="checked" /> Vue
+      <input type="radio" value="React" v-model="checked" /> React
+      <input type="radio" value="Angular" v-model="checked" /> Angular
+    </div>
+    <button type="button" :disabled="!checked" @click="vote">vote</button>
+    <div class="center">
+  
+    <h1>results</h1>
+    <p v-for="(value, key) of results" :key="key">{{ key }}: {{ value }}</p>
   </div>
+  </form>
+  <div class="center">
+    <p><span v-for="(value, key) of votes" :key="key"> {{ value.text_field }}<br/></span></p>
+  </div>
+ </div> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      checked: "",
+      results: {},
+      votes: [],
+    };
+  },
+  methods: {
+    vote() {
+      if (!localStorage.getItem("vote-result")) {
+        localStorage.setItem("vote-result", JSON.stringify({}));
+      }
+      const { checked } = this;
+      const results = JSON.parse(localStorage.getItem("vote-result"));
+      if (!Object.prototype.hasOwnProperty.call(results, checked)) {
+        results[checked] = 0;
+      }
+      results[checked]++;
+      localStorage.setItem("vote-result", JSON.stringify(results));
+      this.results = JSON.parse(localStorage.getItem("vote-result"));
+    },
+  },
+  mounted: function() {
+    fetch('http://beta.sheehanweb.com/index.php/wp-json/vote/v1/post')
+      .then((r) => r.json())
+      .then((res) => this.votes = res.map(x => x.acf));
+      console.log('the component is now mounted');
   }
-}
+};
+
 </script>
 
 <style>
@@ -24,5 +63,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.mainQ {
+  padding-top:50px;
+  /* font-size:5rem; */
+}
+.center {
+  text-align:center;
+  margin:0 auto;
+  
+}
+button {
+  border: none;
+  padding:20px 40px;
+  margin:20px auto;
+  display:block;
 }
 </style>
